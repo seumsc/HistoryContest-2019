384a433d5f6cadf119dd2600e83063d96ab583ef
R-shrug <213183580@seu.edu.cn>
2020-09-03 19:25:26 +0800

    删了一些奇怪的黑历史

--- HistoryContest_2019_server/build/config/conf.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Port = 6553;
-//# sourceMappingURL=conf.js.map
--- HistoryContest_2019_server/build/utils/keys.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Key = "secret";
-//# sourceMappingURL=keys.js.map
